aecad6172ce37e3fe17e640d177dfb6dcc2ac04f
cuishirui <344494024@qq.com>
2017-02-17 20:32:13 +0800

    add admin/orders/state_option button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -23,19 +23,19 @@ class Admin::OrdersController < ApplicationController
  def shipped
    @order = Order.find(params[:id])
    @order.deliver!
-   redirect_to back
+   redirect_to :back
  end
 
  def cancel
    @order = Order.find(params[:id])
-   @order.cancell_order!
+   @order.cancel_order!
    redirect_to :back
  end
 
  def return
    @order = Order.find(params[:id])
    @order.return_good!
-   redirect_to :back 
+   redirect_to :back
  end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -20,6 +20,7 @@ before_create :generate_token
     self.update_columns(is_paid: true)
   end
 
+
   include AASM
 
   aasm do
@@ -28,11 +29,12 @@ before_create :generate_token
     state :shipping
     state :shipped
     state :order_cancelled
-    state :good_retured
+    state :good_returned
+
 
     event :make_payment, after_commit: :pay! do
-      transitions from: :order_placed, to: :paid
-    end
+       transitions from: :order_placed, to: :paid
+     end
 
     event :ship do
       transitions from: :paid,         to: :shipping
@@ -43,12 +45,13 @@ before_create :generate_token
     end
 
     event :return_good do
-      transitions from: :shipped,       to: :good_returned
+      transitions from: :shipped,      to: :good_returned
     end
 
-    event :cancell_order do
+    event :cancel_order do
       transitions from: [:order_placed, :paid], to: :order_cancelled
     end
   end
+
 
 end
