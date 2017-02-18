4e0b93018d53afaccd6e8f846ef9c1a012c3387b
cuishirui <344494024@qq.com>
2017-02-18 14:13:37 +0800

    implement category and change index.html

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,10 +9,12 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
     if @product.save
       redirect_to admin_products_path
     else
@@ -46,6 +48,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :image)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
   end
 end
