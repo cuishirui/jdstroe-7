3d02870ba67dc0408395077d6917b593fd79fe99
cuishirui <344494024@qq.com>
2017-02-23 11:25:58 +0800

    change search/html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
     else
       @category_id = Category.find_by(name: params[:category]).id
       @products = Product.where(:category_id => @category_id)
-    end 
+    end
   end
 
   def show
